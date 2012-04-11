f517d2feb60e4d14697a5141d456045fe016e1f1
Conrad Stansbury <chstansbury@gmail.com>
2012-04-10 23:08:44 -0700

    Commented exit.c

--- exit.c
+++ exit.c
@@ -2,6 +2,14 @@
 #include "extglobals.h"
 #include <stdlib.h>
 
+/* Function: prepareForExit
+ * ------------------------
+ * Frees all dynamically allocated memory.
+ * At the moment, the only memory there is to free
+ * is the board. Since chess engines are a complex
+ * breed of beast, any logic we might want to add at exit
+ * can be put here as well.
+ */
 void prepareForExit() {
 	free(pBoard);
 	return;
