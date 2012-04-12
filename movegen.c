4708ef4d9c7c07415162fb994ca3370ec0c47d39
Conrad Stansbury <chstansbury@gmail.com>
2012-04-11 23:25:51 -0700

    Wrote generateTimid, though a lot of could could be refactored out, possibly with callbacks

--- movegen.c
+++ movegen.c
@@ -13,6 +13,144 @@
 // void extractMoves(Board *pBoard);
 
 void generateTimid(Board *pBoard, MoveSet *pMoves) {
+	BitBoard currentPieces = 0;
+	int color = pBoard->nextMove;
+	int origin = 0, shift = 0, destination = 0;
+	Move currentMove;
+	BitBoard generatedMoves = 0;
+	
+	
+	//pawn
+	currentPieces = (color) ?
+				pBoard->position.black.pawn
+			: pBoard->position.white.pawn;
+	
+	while(currentPieces) {
+		shift = LSB(currentPieces);
+		currentPieces >>= shift;
+		origin += shift
+		
+		generatedMoves = pawnTimidBB(pBoard, origin, color);
+		int i = 0;
+		destination = 0;
+		while(generatedMoves) {
+			i = LSB(generatedMoves);
+			generatedMoves >>= i;
+			destination += i;
+			currentMove = move(0, color ? BLACK_PAWN : WHITE_PAWN, origin, destination);
+			writeMove(pMoves, currentMove);
+		}
+	}
+	
+	//bishop
+	currentPieces = (color) ?
+				pBoard->position.black.bishop
+			: pBoard->position.white.bishop;
+			
+	while(currentPieces) {
+		shift = LSB(currentPieces);
+		currentPieces >>= shift;
+		origin += shift
+		
+		generatedMoves = bishopTimidBB(pBoard, origin, color);
+		int i = 0;
+		destination = 0;
+		while(generatedMoves) {
+			i = LSB(generatedMoves);
+			generatedMoves >>= i;
+			destination += i;
+			currentMove = move(0, color ? BLACK_BISHOP : WHITE_BISHOP, origin, destination);
+			writeMove(pMoves, currentMove);
+		}
+	}
+	
+	//knight
+	currentPieces = (color) ?
+				pBoard->position.black.knight
+			: pBoard->position.white.knight;
+			
+	while(currentPieces) {
+		shift = LSB(currentPieces);
+		currentPieces >>= shift;
+		origin += shift
+		
+		generatedMoves = knightTimidBB(pBoard, origin, color);
+		int i = 0;
+		destination = 0;
+		while(generatedMoves) {
+			i = LSB(generatedMoves);
+			generatedMoves >>= i;
+			destination += i;
+			currentMove = move(0, color ? BLACK_KNIGHT : WHITE_KNIGHT, origin, destination);
+			writeMove(pMoves, currentMove);
+		}
+	}
+	
+	//rook
+	currentPieces = (color) ?
+				pBoard->position.black.rook
+			: pBoard->position.white.rook;
+			
+	while(currentPieces) {
+		shift = LSB(currentPieces);
+		currentPieces >>= shift;
+		origin += shift
+		
+		generatedMoves = rookTimidBB(pBoard, origin, color);
+		int i = 0;
+		destination = 0;
+		while(generatedMoves) {
+			i = LSB(generatedMoves);
+			generatedMoves >>= i;
+			destination += i;
+			currentMove = move(0, color ? BLACK_ROOK : WHITE_ROOK, origin, destination);
+			writeMove(pMoves, currentMove);
+		}
+	}
+	
+	//queen
+	currentPieces = (color) ?
+				pBoard->position.black.queen
+			: pBoard->position.white.queen;
+			
+	while(currentPieces) {
+		shift = LSB(currentPieces);
+		currentPieces >>= shift;
+		origin += shift
+		
+		generatedMoves = queenTimidBB(pBoard, origin, color);
+		int i = 0;
+		destination = 0;
+		while(generatedMoves) {
+			i = LSB(generatedMoves);
+			generatedMoves >>= i;
+			destination += i;
+			currentMove = move(0, color ? BLACK_QUEEN : WHITE_QUEEN, origin, destination);
+			writeMove(pMoves, currentMove);
+		}
+	}
+	
+	//king
+	currentPieces = (color) ?
+				pBoard->position.black.king
+			: pBoard->position.white.king;
+			
+	while(currentPieces) {
+		shift = LSB(currentPieces);
+		currentPieces >>= shift;
+		origin += shift
+		
+		generatedMoves = kingTimidBB(pBoard, origin, color);
+		int i = 0;
+		destination = 0;
+		while(generatedMoves) {
+			i = LSB(generatedMoves);
+			generatedMoves >>= i;
+			destination += i;
+			currentMove = move(0, color ? BLACK_KING : WHITE_KING, origin, destination);
+			writeMove(pMoves, currentMove);
+		}
+	}
 	
 	return;
 }
